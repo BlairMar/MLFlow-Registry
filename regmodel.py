c77a65650325cfc160c4aecb4d86c5a25a2f82db
BlairMar <blairmartin@gmail.com>
2021-07-27 23:49:27 +0100

    setup model registry

---
+++ regmodel.py
@@ -0,0 +1,24 @@
+import mlflow
+import joblib
+from sklearn import datasets, linear_model
+
+data = datasets.load_boston()
+X = data["data"]
+y = data["target"]
+
+mlflow.set_tracking_uri('http://127.0.0.1:8002')
+
+# START RUN
+with mlflow.start_run():
+
+    # FIT MODEL
+    model = linear_model.LinearRegression()
+    model.fit(X, y)
+
+    # LOG MODEL TO MODEL REGISTRY
+    # joblib.dump(model, 'saved_model.joblib')
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="sklearn-model",
+        registered_model_name="sklearn-linear-regression",
+    )
